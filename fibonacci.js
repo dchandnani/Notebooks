e60bd3b0328f1887664ebadc5771c78fcfc8455f
dchandnani <dineshc@outlook.com>
2021-11-17 13:28:39 -0800

    Create fibonacci.js

---
+++ fibonacci.js
@@ -0,0 +1,13 @@
+hello = () => { console.log("Hello world!") }
+
+fib = function(numMax){
+    for(var fibArray = [0,1], i=0,j=1,k=0; k<numMax;i=j,j=x,k++ ){
+        x=i+j;
+        fibArray.push(x);
+        console.log(x)
+    }
+}
+
+hello()
+fib(10)
+
